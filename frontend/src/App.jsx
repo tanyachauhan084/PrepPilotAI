b909a808adb23375da1c5a8bd2ca0c39bdf92e7b
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-06-18 08:32:56 +0530

    defined routes for Home and Auth pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Auth from './pages/auth'
 
 const App = () => {
   return (
-    <div>home</div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/auth' element= {<Auth/>}/>
+    </Routes>
   )
 }
 
