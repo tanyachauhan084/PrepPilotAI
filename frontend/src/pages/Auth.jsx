d2a4a48fb402ce1de955c7d5589119f8fdf27254
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-06-18 12:54:33 +0530

    Added heading with AI Smart Interview badge and icon styling

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {motion} from "motion/react"
 import { BsRobot } from 'react-icons/bs';
-
+import IoSparkles from react/icons/io5;
 const Auth = () => {
   return (
      <div className= 'w-full min-h-screen bg-[#f3f3f3] flex items-center justify-center px-6 py-20'>
@@ -18,6 +18,18 @@ initial={{opacity:0 , y:-40}}
                   </div>
                   <h2 className='font-semibold text-lg'>PrepPilot AI</h2>
               </div>
+  
+
+        <h1 className='text-2xl md:text-3xl font-semibold text-center leading-snug mb-4'>
+                      Continue with
+                      <span className='bg-green-100 text-green-600 px-3 py-1 rounded-full inline-flex items-center gap-2'>
+                          <IoSparkles size={16}/>
+                          AI Smart Interview
+      
+                      </span>
+                  </h1>
+      
+   
   
 
 </motion.div>
