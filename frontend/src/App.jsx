40a6e12ca63ed7fd5ab87a9d84d7c6704e02bf2c
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-06-19 10:40:33 +0530

    added interviewhistory route in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Auth from './pages/Auth'
+import InterviewHistory from './pages/InterviewHistory'
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/auth' element= {<Auth/>}/>
+      <Route path='/interviewhistory' element={<InterviewHistory/>}/>
     </Routes>
   )
 }
