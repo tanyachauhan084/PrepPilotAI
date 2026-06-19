e76bf33e2a73c3216ca1826f8c6c0d00819a60ad
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-06-19 09:50:20 +0530

    added responsive page container layout

---
+++ frontend/src/pages/InterviewHistory.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const InterviewHistory = () => {
+  return (
+     <div className='min-h-screen bg-linear-to-br from-gray-50 to-emerald-50 py-10' >
+            <div className='w-[90vw] lg:w-[70vw] max-w-[90%] mx-auto'>
+
+              </div>
+            </div>
+  )
+}
+
+export default InterviewHistory
