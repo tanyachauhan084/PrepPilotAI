714ec163fe6296dbcf002ad12f3a86543c6ea7e4
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-06-19 12:12:18 +0530

    added auth utility in the client folder for third party auth

---
+++ frontend/src/utils/auth.js
@@ -0,0 +1,20 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider } from "firebase/auth"
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: "preppilotai.firebaseapp.com",
+  projectId: "preppilotai",
+  storageBucket: "preppilotai.firebasestorage.app",
+  messagingSenderId: "404850320911",
+  appId: "1:404850320911:web:4fb573e37c3c97cec3a57a"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getauth(app);
+
+const provider= new GoogleAuthProvider()
+
+
+export {auth, provider};
