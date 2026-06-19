c4510c5c95c11a21545e4f6b75cf4ecba9c8453a
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-06-19 09:53:38 +0530

    added back navigation button

--- frontend/src/pages/InterviewHistory.jsx
+++ frontend/src/pages/InterviewHistory.jsx
@@ -5,6 +5,12 @@ const InterviewHistory = () => {
      <div className='min-h-screen bg-linear-to-br from-gray-50 to-emerald-50 py-10' >
             <div className='w-[90vw] lg:w-[70vw] max-w-[90%] mx-auto'>
 
+  <div className='mb-10 w-full flex items-start gap-4 flex-wrap'>
+                      <button
+                          onClick={() => navigate("/")}
+                          className='mt-1 p-3 rounded-full bg-white shadow hover:shadow-md transition'><FaArrowLeft className='text-gray-600' /></button>
+  
+</div>
               </div>
             </div>
   )
