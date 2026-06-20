f10d7052f2a9f389df80bf38344153892555e0f5
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-06-20 12:34:17 +0530

    added the  healthcheck controller

---
+++ backend/src/controllers/healthcheck.controllers.js
@@ -0,0 +1,17 @@
+import ApiReponse from "../utilities/api-response.js";
+import asyncHandler from "../utilities/async_handler.js";
+
+
+const healthcheck= asyncHandler(async(req,res)=>{
+
+    return res.status(200).json(
+        new ApiReponse(
+            200,
+            "this is a healthcheck",
+            "everything is great here"
+        )
+    )
+})
+
+
+
