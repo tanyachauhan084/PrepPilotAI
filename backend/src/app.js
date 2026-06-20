699db3e2c870dbf92a6b7d5cce95d46c89b13935
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-06-20 12:47:38 +0530

    mount healthCheck router

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,14 @@
 import express from "express";
+import healthcheckroute from "./routes/healthcheck.routes";
 
 const server= express();
 //built-in middlewares//
 server.use(express.json({limit:"16kb"}));
 server.use(express.urlencoded({limit:"16kb", extended:true}))
 server.use(express.static("public"))
+
+
+server.use("/healthcheck", healthcheckroute );
 
 export default server
 
