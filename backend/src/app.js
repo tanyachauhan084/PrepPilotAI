17bc5f06cd8aadc805a5ef755f886782623637d9
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-06-21 12:50:44 +0530

    added cors configuration with origin and credentials in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,21 @@ import express from "express";
 import healthcheckroute from "./routes/healthcheck.routes.js";
 import authroutes from "./routes/auth.routes.js";
 import errorMiddleware from "./middlewares/error.middlewares.js";
+import cors from "cors"
+
 
 const server= express();
 //built-in middlewares//
 server.use(express.json({limit:"16kb"}));
 server.use(express.urlencoded({limit:"16kb", extended:true}))
 server.use(express.static("public"))
+
+//cross-origin configuration//
+server.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+   
+}))
 
 
 server.use("/healthcheck", healthcheckroute );
