30b92ae4e8d40ee6f3cefd7ec7ef15829e179a1c
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-06-22 13:55:58 +0530

    fetched current user on component mount

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+
+import axios from 'axios'
 import Auth from './pages/Auth'
 import InterviewHistory from './pages/InterviewHistory'
+import { linkWithCredential } from 'firebase/auth'
+
+
 
 
 
 export const serverUrl= "http://localhost:8880"
 
 const App = () => {
+
+ useEffect(() => {
+   
+  const getUser= async(req,res)=>{
+
+    try{
+
+      const result= await axios.get(serverUrl,+ "/user/info", {withCredentials:true});
+      console.log(result.data);
+    }
+
+    catch(error){
+
+      console.log(error.response.data);
+    }
+  }
+ 
+
+  getUser();
+  
+ }, [])
+ 
+
+
+    
+    
+  
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
@@ -16,6 +48,6 @@ const App = () => {
       <Route path='/interviewhistory' element={<InterviewHistory/>}/>
     </Routes>
   )
-}
 
+}
 export default App
