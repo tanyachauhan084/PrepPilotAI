44804997875490a2f48ea025a209fd1a1e5b02ea
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-06-22 11:48:28 +0530

    added cookie-parser

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,9 +3,13 @@ import healthcheckroute from "./routes/healthcheck.routes.js";
 import authroutes from "./routes/auth.routes.js";
 import errorMiddleware from "./middlewares/error.middlewares.js";
 import cors from "cors"
+import cookieParser from "cookie-parser"
 
 
 const server= express();
+
+server.use(cookieParser());
+
 //built-in middlewares//
 server.use(express.json({limit:"16kb"}));
 server.use(express.urlencoded({limit:"16kb", extended:true}))
