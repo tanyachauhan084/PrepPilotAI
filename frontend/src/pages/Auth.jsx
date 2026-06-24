1aa28dfe4d1e404edf7aff84db6295e59cc0d66c
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-06-24 13:48:50 +0530

    added modal mode support using isModel prop

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -27,12 +27,23 @@ const Auth = ({isModel= false}) => {
            }
     }
   return (
-     <div className= 'w-full min-h-screen bg-[#f3f3f3] flex items-center justify-center px-6 py-20'>
+       <div className={`
+      w-full 
+      ${isModel ? "py-4" : "min-h-screen bg-[#f3f3f3] flex items-center justify-center px-6 py-20"}
+    `}>
 <motion.div
 initial={{opacity:0 , y:-40}} 
         animate={{opacity:1 , y:0}} 
         transition={{duration:1.05}}
-        className='w-full max-w-lg p-12 rounded-4xl bg-white shadow-2xl border border-gray-200'>
+
+        
+          className={`
+        w-full 
+        ${isModel ? "max-w-md p-8 rounded-3xl" : "max-w-lg p-12 rounded-4xl"}
+        bg-white shadow-2xl border border-gray-200
+      `
+
+          }>
 
      <div className='flex items-center justify-center gap-3 mb-6'>
                   <div className='bg-black text-white p-2 rounded-lg'>
