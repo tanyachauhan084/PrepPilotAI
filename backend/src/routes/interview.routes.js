7af383eb8ba713da70d837ba66e0ea3abf70bf01
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-06-25 14:18:01 +0530

    corrected authmiddleware import in the interview-Router

--- backend/src/routes/interview.routes.js
+++ backend/src/routes/interview.routes.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { analyzeResume } from "../controllers/interview.controllers.js";
+import authMiddleware from "../middlewares/auth.middlewares.js";
 
 export const interviewRouter= express.Router();
 
-interviewRouter.post("/resume", isAuth, upload.single("resume"), analyzeResume)
+interviewRouter.post("/resume", authMiddleware, upload.single("resume"), analyzeResume)
