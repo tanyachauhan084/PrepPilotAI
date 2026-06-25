c2a5ce0fee80d84062c316bff86d73a276c550e0
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-06-25 14:10:57 +0530

    mounted interview router on app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ import errorMiddleware from "./middlewares/error.middlewares.js";
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import userRouter from "./routes/user.routes.js";
+import { interviewRouter } from "./routes/interview.routes.js";
 
 
 const server= express();
@@ -31,6 +32,10 @@ server.use("/auth", authroutes);
 
 
 server.use("/user", userRouter);
+
+
+server.use("/interview", interviewRouter);
+
 
 server.use(errorMiddleware);
 
