534d773aa717acfce0df455956f164e372848ad5
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-06-25 11:50:13 +0530

    created InterviewPage component with step state

---
+++ frontend/src/pages/InterviewPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useState } from 'react'
+
+function InterviewPage() {
+    const [step,setStep] = useState(1)
+
+
+
+
+    return (
+    <div className='min-h-screen bg-gray-50'>
+       
+       
+    </div>
+  )
+}
+
+export default InterviewPage
