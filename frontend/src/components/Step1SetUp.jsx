a029120aec351e5f13be9b5ace960f10f612ecaa
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-06-26 11:32:55 +0530

    added interview introduction section

--- frontend/src/components/Step1SetUp.jsx
+++ frontend/src/components/Step1SetUp.jsx
@@ -10,7 +10,26 @@ const Step1SetUp = ({onStart}) => {
    
                <div className='w-full max-w-6xl bg-white rounded-3xl shadow-2xl grid md:grid-cols-2 overflow-hidden'>
    
-                  
+    <motion.div>
+                      initial={{ x: -80, opacity: 0 }}
+                      animate={{ x: 0, opacity: 1 }}
+                      transition={{ duration: 0.7 }}
+                      className='relative bg-linear-to-br from-green-50 to-green-100 p-12 flex flex-col justify-center'>
+  
+                      <h2 className="text-4xl font-bold text-gray-800 mb-6">
+                          Start Your AI Interview
+                      </h2>
+  
+                      <p className="text-gray-600 mb-10">
+                          Practice real interview scenarios powered by AI.
+                          Improve communication, technical skills, and confidence.
+                      </p>
+  
+                      <div className='space-y-5'>
+
+              </div>
+
+              </motion.div>
               
                </div>
    
@@ -19,4 +38,4 @@ const Step1SetUp = ({onStart}) => {
    }
  
 
-export default Step1SetUp
+export default Step1SetUp.
