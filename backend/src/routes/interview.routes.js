91f0fc9eef0977cf3b6803a94d7d7875b0cb9d6d
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-06-30 11:42:53 +0530

    added questions route and updated a few import paths

--- backend/src/routes/interview.routes.js
+++ backend/src/routes/interview.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { upload } from "../middlewares/multer.middlewares.js";
-import { analyzeResume } from "../controllers/interview.controllers.js";
+import { analyzeResume, finishInterview,  generateQuestion,  submitAnswer } from "../controllers/interview.controllers.js";
 import authMiddleware from "../middlewares/auth.middlewares.js";
+
 
 export const interviewRouter= express.Router();
 
-interviewRouter.post("/resume", authMiddleware, upload.single("resume"), analyzeResume)
+interviewRouter.post("/resume", authMiddleware, upload.single("resume"), analyzeResume);
+interviewRouter.post("/questions", authMiddleware, generateQuestion);
