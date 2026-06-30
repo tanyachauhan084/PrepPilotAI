2a6f325c411b286e11dd42b29b86a372e3f81666
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-06-30 12:56:17 +0530

    Added interview video section

--- frontend/src/components/Step2Interview.jsx
+++ frontend/src/components/Step2Interview.jsx
@@ -10,7 +10,26 @@ const Step2Interview = ({interviewData, onFinish}) => {
  <div className='min-h-screen bg-linear-to-br from-emerald-50 via-white to-teal-100 flex items-center justify-center p-4 sm:p-6'>
       <div className='w-full max-w-350 min-h-[80vh] bg-white rounded-3xl shadow-2xl border border-gray-200 flex flex-col lg:flex-row overflow-hidden'>
 
-       </div>
+
+
+        {/* video section */}
+        <div className='w-full lg:w-[35%] bg-white flex flex-col items-center p-6 space-y-6 border-r border-gray-200'>
+          <div className='w-full max-w-md rounded-2xl overflow-hidden shadow-xl'>
+            <video
+              src={videoSource}
+              key={videoSource}
+              ref={videoRef}
+              muted
+              playsInline
+              preload="auto"
+              className="w-full h-auto object-cover"
+            />
+          </div>
+
+
+</div>    
+
+   </div>
 
     </div>
 
