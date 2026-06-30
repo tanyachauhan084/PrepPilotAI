407be1fe0717254a6aec1a7dabb8f95e20ce8f52
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-06-30 12:51:39 +0530

    Initialized interview page layout

---
+++ frontend/src/components/Step2Interview.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Step2Interview = ({interviewData, onFinish}) => {
+ 
+
+  const {interviewId, questions, userName}= interviewData;
+ 
+  return (
+    <div>
+ <div className='min-h-screen bg-linear-to-br from-emerald-50 via-white to-teal-100 flex items-center justify-center p-4 sm:p-6'>
+      <div className='w-full max-w-350 min-h-[80vh] bg-white rounded-3xl shadow-2xl border border-gray-200 flex flex-col lg:flex-row overflow-hidden'>
+
+       </div>
+
+    </div>
+
+    </div>
+  )
+}
+
+export default Step2Interview
