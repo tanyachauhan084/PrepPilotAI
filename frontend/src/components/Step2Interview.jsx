16cb45233625529ad2703f44df5d4051e04fb502
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-07-01 12:22:07 +0530

    added interview timer section

--- frontend/src/components/Step2Interview.jsx
+++ frontend/src/components/Step2Interview.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import maleVideo from "../assets/videos/male-ai.mp4"
 import femaleVideo from "../assets/videos/female-ai.mp4"
-
+import Timer from './Timer'
 const Step2Interview = ({interviewData, onFinish}) => {
  
 
   const {interviewId, questions, userName}= interviewData;
  
   return (
-    <div>
+   
  <div className='min-h-screen bg-linear-to-br from-emerald-50 via-white to-teal-100 flex items-center justify-center p-4 sm:p-6'>
       <div className='w-full max-w-350 min-h-[80vh] bg-white rounded-3xl shadow-2xl border border-gray-200 flex flex-col lg:flex-row overflow-hidden'>
 
@@ -30,14 +30,37 @@ const Step2Interview = ({interviewData, onFinish}) => {
           {/* subtitle pending */}
        
 
+
+      
+      
+                {/* timer Area */}
+                <div className='w-full max-w-md bg-white border border-gray-200 rounded-2xl shadow-md p-6 space-y-5'>
+                  <div className='flex justify-between items-center'>
+                    <span className='text-sm text-gray-500'>
+                      Interview Status
+                    </span>
+                  <span className='text-sm font-semibold text-emerald-600'>
+                      AI Speaking
+                    </span>
+                  </div>
+      
+                  <div className="h-px bg-gray-200"></div>
+      
+                  <div className="flex justify-center">
+      
+                    <Timer timeLeft={30} totalTime= {60} />
+                  </div>
+      
+                  
+            
+                </div>
+              </div>
+      
+
 </div>    
-
-   </div>
-
-    </div>
 
     </div>
   )
 }
 
-export default Step2Interview
+export default Step2Interview;
