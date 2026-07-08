c7038fe500714505f978d285998dc86bd38866c2
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-07-08 14:16:36 +0530

    spelling correction

--- frontend/src/pages/Pricing.jsx
+++ frontend/src/pages/Pricing.jsx
@@ -6,7 +6,7 @@ const Pricing = () => {
     <div className="pt-8 pl-10">
 🚧 MVP Notice
 <br />
-  PrepPilot AI currently focuses on the core interview experience. Pricing and subscription management are intentionally excluded from this MVP.
+  PrepPilotAI currently focuses on the core interview experience. Pricing and subscription management are intentionally excluded from this MVP.
 
     </div>
   )
