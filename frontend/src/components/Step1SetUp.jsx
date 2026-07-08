8c6d484e8c53433730b16b0b50dcb5539280ed56
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-07-08 14:12:53 +0530

    Spelling correction

--- frontend/src/components/Step1SetUp.jsx
+++ frontend/src/components/Step1SetUp.jsx
@@ -280,7 +280,7 @@ const Step1SetUp = ({onStart}) => {
                                 whileHover={{ scale: 1.03 }}
                                 whileTap={{ scale: 0.95 }}
                                 className='w-full disabled:bg-gray-600 bg-green-600 hover:bg-green-700 text-white py-3 rounded-full text-lg font-semibold transition duration-300 shadow-md'>
-                                {loading ? "Staring...":"Start Interview"}
+                                {loading ? "Starting...":"Start Interview"}
     
     
                             </motion.button>
